fdf0a8af8294a8cbf16e7e4eb96893aa1cc30a63
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2023-11-02 16:28:37 +0800

    updated the fetchCatFact function and added it in useEffect function

--- fetch-from-api/src/App.js
+++ fetch-from-api/src/App.js
@@ -5,15 +5,19 @@ import { useEffect, useState } from "react";
 function App() {
   const [catFact, setCatFact] = useState("");
 
-  useEffect(() => {
+  const fetchCatFact = () => {
     Axios.get("https://catfact.ninja/fact").then((res) => {
       setCatFact(res.data.fact);
     });
+  }
+
+  useEffect(() => {
+    fetchCatFact();
   }, []);
 
   return (
     <div className="App">
-      <button> Generate Cat Fact </button>
+      <button onClick={fetchCatFact}> Generate Cat Fact </button>
       <p>{catFact}</p>
     </div>
   );
