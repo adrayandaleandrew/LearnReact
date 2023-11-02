c6ffb48b541a1f229c47d70bf61a8ed66a42ae61
Dale Andrew Adrayan <adrayandaleandrew@gmail.com>
2023-11-02 19:17:13 +0800

    added code for exercise

--- fetch-from-api/src/App.js
+++ fetch-from-api/src/App.js
@@ -56,15 +56,25 @@ import { useEffect, useState } from "react";
 
 // EXERCISE
 function App() {
-  
+
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
 
   return (
     <div className="App">
-      <h1>Generate An Excuse</h1>
-      <button> Party </button>
-      <button> Family </button>
-      <button> Office </button>
-     
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p> {generatedExcuse} </p>
     </div>
   );
 }
